69fc60aeca5bb45423de53b22af62f4a1b04ed3e
TyronNIKO <niko.fm@gmail.com>
2026-02-14 08:20:33 +0200

    final version

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    // base: "/BeMyValentine/",
+    base: "/BeMyValentine/",
     plugins: [tailwindcss()],
 });
